6a026b2e5c5223075aba4c4a65c7d5618a3c32a0
Zalahari <Zalaharih@gmail.com>
2013-07-31 23:05:15 +0200

    Changed menu title
    Added a whitespace (&#160;) after the menu title. Just to see how GitHub
    works.

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <p>
-			Admin Menu:
+			Admin Menu:&#160;
 			<a href="admin/addUser.php">addUser</a> | 
 			<a href="admin/addAnimal.php">addAnimal</a> | 
 			<a href="admin/addAnimalColors.php">addAnimalColors</a> | 
